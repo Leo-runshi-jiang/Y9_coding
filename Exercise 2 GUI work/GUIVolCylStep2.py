926edd7100ef9ebca349cf04a589c829e39f1627
Leo Jiang <leo.jiang@GCCYJPREJ1WK.local>
2019-09-25 13:49:42 -0400

    Finnished step 2

--- Exercise 2 GUI work/GUIVolCylStep2.py
+++ Exercise 2 GUI work/GUIVolCylStep2.py
@@ -27,5 +27,39 @@ def calcVolCylinder(radius, height):
 #main Program
 root = tk.Tk()
 #Building widgets goes before mainloop.
+#name title 
+title = tk.Label(root, text = "Cylinder Volume Calculator")
+#colour
+title.config(fg = "white", bg = "pink")
+#position (pack is a placment function)
+title.pack(fill = tk.BOTH)
+#radius title, W=west
+radiusLabel = tk.Label(root, text = "Radius:")
+radiusLabel.config(anchor = tk.W)
+radiusLabel.pack(fill = tk.BOTH)
+#input box for radius
+radiusEntry = tk.Entry(root)
+radiusEntry.config()
+radiusEntry.pack(fill = tk.BOTH)
+#same for height
+heightLabel = tk.Label(root, text = "Height:")
+heightLabel.config(anchor = tk.W)
+heightLabel.pack(fill = tk.BOTH)
+
+heightEntry = tk.Entry(root)
+heightEntry.config()
+heightEntry.pack(fill = tk.BOTH)
+#output
+output = tk.Text(root)
+output.config(width = 50, height = 10, state = "disabled", borderwidth = 2, relief = "groove")
+output.pack()
+
+btnrun = tk.Button(root, text = "CALCULATE", highlightbackground='#3E4149')
+btnrun.config(fg="blue")
+btnrun.pack(fill = tk.BOTH)
+
+check = tk.Checkbutton(root, text = "High Contrast")
+check.config(anchor = tk.W)
+check.pack(fill = tk.BOTH)
 root.mainloop()
 print("End Program")
