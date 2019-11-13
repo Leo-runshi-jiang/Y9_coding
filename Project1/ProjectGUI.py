bfe972d0f06f2b2f755abd3c8d2da5838833d090
Leo Jiang <leo.jiang@GCCYJPREJ1WK.local>
2019-11-13 14:01:13 -0500

    GUI improvment

--- Project1/ProjectGUI.py
+++ Project1/ProjectGUI.py
@@ -1,6 +1,17 @@
 import tkinter as tk
 
 root = tk.Tk()
+
+primeclr = '#F9D8D7'
+
 toolbar = tk.Label(root)
-toolbar.config(bg = '#F9D8D7', width= 30)
-toolbar.pack(fill = tk.BOTH)
+toolbar.config(bg = primeclr, width= 50)
+toolbar.grid(row = 0)
+
+searchbtn = tk.Button(root, text = "SEARCH")
+searchbtn.config(width = 30, height = 2,anchor = tk.W)
+searchbtn.grid(column = 0, row = 1)
+
+searchdropdwn = tk.Button(root)
+searchdropdwn.config(width = 20, height = 2,anchor = tk.E)
+searchdropdwn.grid(column = 0, row = 1)
---
+++ Project1/tester.py
@@ -0,0 +1,5 @@
+s = 0
+n = 9
+for i in range(0,n,1):
+	b=i
+print(b)
