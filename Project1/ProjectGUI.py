9f423dfc5c1c80e7c3dbbb9d9441fd699e045bd0
Leo Jiang <leo.jiang@GCCYJPREJ1WK.local>
2019-11-25 13:42:53 -0500

    Started on backend

--- Project1/ProjectGUI.py
+++ Project1/ProjectGUI.py
@@ -5,6 +5,88 @@ secondaryclr = '#FFFFFF'
 rootbgclr = '#EFEFEF'
 borderclr = '#999999'
 
+#When you load your program you are going to have to load all your formulas from file. 
+
+
+#Functions
+#Recommendation:
+#Create physform.txt with a handfull of formulas
+#Write teh code right below that reads the file and prints them to the screen. 
+
+
+def processValues(a,b,c,d,e):
+	print("Processing possible formulas")
+
+
+
+def searchFNC(*args):
+	print("BUTTON PRESSED")
+
+	#Question: What are the conditions around what is in the entry
+
+	
+	#step 1: 
+	#I assume all values are invalid
+	check = [False,False,False,False,False]
+
+	#Step 2: Get all the values
+	v1 = entry1.get()
+	v2 = entry2.get()
+	v3 = entry3.get()
+	v4 = entry4.get()
+	v5 = entry5.get()
+
+	#Step 3: try to cast to an int and if that fails we know it is valid
+	try:
+		v1 = int(v1) #If this is an in!!!
+		
+	except:
+		print("It is a letter")
+		check[0] = True
+
+
+	try:
+		v2 = int(v2) #If this is an in!!!
+		
+	except:
+		print("It is a letter")
+		check[1] = True
+
+	try:
+		v3 = int(v3) #If this is an in!!!
+		
+	except:
+		print("It is a letter")
+		check[2] = True
+
+	try:
+		v4 = int(v4) #If this is an in!!!
+		
+	except:
+		print("It is a letter")
+		check[3] = True
+
+	try:
+		v5 = int(v5) #If this is an in!!!
+		
+	except:
+		print("It is a letter")
+		check[4] = True
+
+
+	runprocess = True
+
+	for i in range(0,len(check),1):
+		if (check[i] == False):
+			runprocess = False
+
+	if runprocess == True:
+		processValues(v1,v2,v3,v4,v5)
+	
+
+
+
+
 root = tk.Tk()
 
 root.config(bg= rootbgclr)
@@ -13,11 +95,12 @@ toolbar = tk.Label(root)
 toolbar.config(bg = primeclr, width= 50)
 toolbar.place(x=0, y=0)
 
-searchbtn = tk.Button(root, text = "SEARCH")
+searchbtn = tk.Button(root, text = "SEARCH", command = searchFNC)
 searchbtn.config(width = 15, height = 2)
-searchbtn.place(x=25,y=25)
+searchbtn.place(x=20,y=25)
 
 optionlist = [
+"all"
 "math",
 "algebric equations", 
 "geometry and trig", 
